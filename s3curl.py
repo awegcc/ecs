d391d9babd77a16ec1e3eae4491e6f814e02cc3e
Chen, Carlos <carlos.chen@emc.com>
2017-10-29 16:01:35 -1100

    add argparse

--- s3curl.py
+++ s3curl.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+# As of python 2.7, optparse is deprecated
+import argparse
 
 def main():
-    print "Hello python"
+    ''' main function '''
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--id", default=sys.argv[0], help="id")
+
+	args = parser.parse_args()
+
     
 if __name__ == '__main__':
     main()
