4d498f34e4cef52e9064c18bf3b6fc0e032b46df
dawtercc <dawtercc@gmail.com>
2017-12-04 03:31:09 +0000

    fix datelist

--- check_obscan.sh
+++ check_obscan.sh
@@ -22,9 +22,10 @@ done
 datelist="${datelist}$(date +'.%Y%m%d*,}')"
 datenow=$(date '+%Y%m%d')
 
-echo viprexec -i -c "zgrep 'This\|candidateCount' /var/log/blobsvc-chunk-reclaim.log${datelist}"
-viprexec -i -c "zgrep candidateCount /var/log/blobsvc-chunk-reclaim.log${datelist}" > candidateCount.log.${datenow}
-viprexec -i -c "zgrep This /var/log/blobsvc-chunk-reclaim.log${datelist}" > roundEnd.log.${datenow}
+echo viprexec -i -c "zgrep -h candidateCount /var/log/blobsvc-chunk-reclaim.log${datelist}"
+viprexec -i -c "zgrep -h candidateCount /var/log/blobsvc-chunk-reclaim.log${datelist}" > candidateCount.log.${datenow}
+echo viprexec -i -c "zgrep -h This /var/log/blobsvc-chunk-reclaim.log${datelist}"
+viprexec -i -c "zgrep -h This /var/log/blobsvc-chunk-reclaim.log${datelist}" > roundEnd.log.${datenow}
 
 
 for((index=0;index<128;index++))
@@ -33,7 +34,7 @@ do
     candidateCount=$(grep ${rg}_${ob} candidateCount.log.${datenow} | grep REPO_SCAN | grep "Saving" | tail -n 1 | awk -F 'candidateCount ' '{print $2}' | awk -F ',' '{print $1}')
     failedCandidateCount=$(grep ${rg}_${ob} candidateCount.log.${datenow}  | grep REPO_SCAN | grep Saving | tail -n 1 | awk -F 'failedCandidateCount ' '{print $2}' | awk -F ',' '{print $1}')
     lastTaskTime=$(grep ${rg}_${ob} candidateCount.log.${datenow} | grep REPO_SCAN | grep Saving | tail -n 1 | awk -F 'lastTaskTime ' '{print $2}' | awk -F ' |\n|\r' '{print $1}')
-    lastEndtime=$(grep ${rg}_${ob}  roundEnd.log.${datenow} | grep REPO_SCAN | grep round | tail -n 1 | awk -F '2017-' '{print $2}' | awk -F ',' '{print $1}')
+    lastEndtime=$(grep ${rg}_${ob}  roundEnd.log.${datenow} | grep REPO_SCAN | grep round | tail -n 1 | awk '{print substr($1,0,19)}')
     lastDuration=$(grep ${rg}_${ob} roundEnd.log.${datenow} | grep REPO_SCAN | grep round | tail -n 1 | awk '{print $17}')
     printf "%13s candidateCount %9s, failedCandidateCount %6s, lastTaskTime %13s, lastEndtime %19s, lastDuration %11s\n"\
         $ob $candidateCount $failedCandidateCount $lastTaskTime $lastEndtime $lastDuration
