2670965772be9e135c943e901869e7e6d7a18cbf
dawtercc <dawtercc@gmail.com>
2017-12-26 10:33:39 +0000

    add args

--- get_mgmt_info.sh
+++ get_mgmt_info.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 #
 
-eval $(netstat -ntl | awk '{if($4~":4443" && $4!~"127.0.0.1"){printf "mgmt_addr=%s\n",$4}else if($4~":9101"){printf "dt_addr=%s\n",$4}}')
+if [[ $# < 1 ]]
+then
+    eval $(netstat -ntl | awk '{if($4~":4443" && $4!~"127.0.0.1"){printf "mgmt_addr=%s\n",$4}else if($4~":9101"){printf "dt_addr=%s\n",$4}}')
+else
+    mgmt_addr="$1:9101"
+fi
 
 eval $(curl -i -s -L --location-trusted -k https://${mgmt_addr}/login -u emcservice:ChangeMe | awk '/X-SDS-AUTH-TOKEN:/{printf "SDS_TOKEN=\047%s\047\n",$0}')
 if [ "x$SDS_TOKEN" == "x" ]
