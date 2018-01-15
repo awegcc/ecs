c0101552a3d771f2d0c942f90f649226ff559dde
dawtercc <dawtercc@gmail.com>
2018-01-15 02:20:00 +0000

    refine again

--- gc_verification_status.sh
+++ gc_verification_status.sh
@@ -3,8 +3,11 @@
 if [[ $# == 1 ]]
 then
     days=$1
+elif [[ $# == 0 ]]
+then
+    days=0
 else
-    echo "Usage: $0 days"
+    echo "Usage: $0 back_forward_days"
     exit 1
 fi
 
@@ -17,21 +20,21 @@ do
         datelist="$datelist$day"
 done
 datelist="${datelist}$(date +'.%Y%m%d*,}')"
-datenow=$(date '+%Y%m%d')
-dump_data=dump_data.${datenow}
+dump_data=dump_data.$(date '+%Y%m%d')
 
 # [TaskScheduler-BlobService-REPO_SCAN-ScheduledExecutor-005] DEBUG  ChunkReferenceScanner.java (line 147) Saving checkpoint for scanner REPO:urn:storageos:OwnershipInfo:d3cbdbfe-7590-4b34-afd4-4a0490dbe7cc_e8c3dc6f-cac7-40f4-b0c7-de94fd0ef724_OB_98_128_0:: candidateCount 2, failedCandidateCount 0, lastTaskTime 1515577721063
 # [TaskScheduler-BlobService-REPO_SCAN-ScheduledExecutor-000]  INFO  RepoChunkReferenceScanner.java (line 217) This round GC verification of scanner:REPO:urn:storageos:OwnershipInfo:d3cbdbfe-7590-4b34-afd4-4a0490dbe7cc_5af20804-aec5-4eab-8899-df8cc7aa5b45_OB_40_128_0: last: 338941 milliseconds for objectScanCount 123928
 
 echo viprexec -c "zgrep -h \"This\\|candidateCount\" /opt/emc/caspian/fabric/agent/services/object/main/log/blobsvc-chunk-reclaim.log${datelist}"
-viprexec "zgrep -h \"This\\|candidateCount\" /opt/emc/caspian/fabric/agent/services/object/main/log/blobsvc-chunk-reclaim.log${datelist}" > ${dump_data}
+#viprexec "zgrep -h \"This\\|candidateCount\" /opt/emc/caspian/fabric/agent/services/object/main/log/blobsvc-chunk-reclaim.log${datelist}" > ${dump_data}
 
 awk -v type=$TYPE '$2~type{
                   if($4=="ChunkReferenceScanner.java" && $7=="Saving") {
-                      dtid=substr($11,index($11,"_")+1)
-                      dtid=substr(dtid,1,index(dtid,":"))
                       for(i=1;i<=NF;i++){
-                          if($i=="candidateCount"){
+                          if($i~":OwnershipInfo:") {
+                              dtid=substr($i,index($i,"_")+1)
+                              dtid=substr(dtid,1,index(dtid,":")-1)
+                          } else if($i=="candidateCount"){
                               array[dtid][$i]=$(i+1);
                           } else if($i=="failedCandidateCount"){
                               array[dtid][$i]=$(i+1);
@@ -40,27 +43,28 @@ awk -v type=$TYPE '$2~type{
                           }
                       }
                   }
-                  else if($4=="RepoChunkReferenceScanner.java" && $7=="This") {
-                      dtid=substr($11,index($11,"_")+1)
-                      dtid=substr(dtid,1,index(dtid,":"))
+                  else if($4=="RepoChunkReferenceScanner.java" && ($7=="This" || $8=="This")) {
                       for(i=1;i<=NF;i++){
-                          if($i=="milliseconds") {
+                          if($i~":OwnershipInfo:") {
+                              dtid=substr($i,index($i,"_")+1)
+                              dtid=substr(dtid,1,index(dtid,":")-1)
+                          } else if($i=="milliseconds") {
                               array[dtid][$i]=$(i-1);
-                              array[dtid]["roundtime"]=substr($1,0,19);
                           }
                       }
+                      array[dtid]["lastEndtime"]=substr($1,index($1,"T")-5)
                   }
     }
     END{
-        printf("%48s\tcandidateCount\tfailedCandidateCount\tlastTaskTime\tlastEndtime\tlastDuration (hrs)\n","dtId")
+        printf("%50s\tcandidateCount\tfailedCandidateCount\tlastTaskTime\t%19s\tlastDuration (hrs)\n","dtId","lastEndtime")
         for(k1 in array) {
-            printf("%s\t%14s\t%14s\t%d\t%s\t%s\t%.2f\n",k1,\
+            printf("%50s\t%15s\t%21s\t%13s\t%19s\t%13s\t%.2f\n",k1,\
                                                             array[k1]["candidateCount"],\
                                                             array[k1]["failedCandidateCount"],\
                                                             array[k1]["lastTaskTime"],\
-                                                            array[k1]["roundtime"],\
+                                                            array[k1]["lastEndtime"],\
                                                             array[k1]["milliseconds"],\
-                                                            array[k1]["milliseconds"]/3600);
+                                                            array[k1]["milliseconds"]/3600000);
         }
     }' $dump_data
 
