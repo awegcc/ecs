487b89017dde0132c70698761b5c015f2a2ba46e
dawtercc <dawtercc@gmail.com>
2018-01-17 08:42:23 +0000

    add hours

--- datelist.sh
+++ datelist.sh
@@ -7,7 +7,16 @@ do
 	day=$(date +'.%Y%m%d*,' -d"$i day ago")
 	datelist="$datelist$day"
 done
-
 datelist="${datelist}$(date +'.%Y%m%d*}')"
 
 echo "$datelist"
+
+hourlist='{'
+for((i=days*24;i>0;i--))
+do
+	day=$(date +'.%Y%m%d-%H*,' -d"$i hour ago")
+	hourlist="$hourlist$day"
+done
+hourlist="${hourlist}$(date +'.%Y%m%d-%H*}')"
+
+echo "$hourlist"
