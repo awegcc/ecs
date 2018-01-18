c2685dd81a78cc290f8e01772af562cb750df43d
dawtercc <dawtercc@gmail.com>
2018-01-18 05:52:59 +0000

    Add if

--- gc_reclaim_history.sh
+++ gc_reclaim_history.sh
@@ -6,54 +6,32 @@ MACHINES=MACHINES
 function search_logs
 {
     echo -e "line:${LINENO} ${FUNCNAME[0]} - Start"
-    local log_file=$1
+    local log_file=${1}*
     local within_days=$2
     local key_words="$3"
     local output_file=$4
     local log_path="/opt/emc/caspian/fabric/agent/services/object/main/log/"
-    set -x
-    xargs -a ${MACHINES} -I NODE -P0 sh -c 'ssh $1 find $2 -maxdepth 1 -name ${3}* -exec zgrep -h $4 {} \; > $5-$1'  -- NODE $log_path $log_file $key_words ${output_file}
-    set +x
+    xargs -a ${MACHINES} -I NODE -P0 sh -c 'ssh NODE "find $1 -maxdepth 1 -mtime -$2 -name \"$3\" -exec zgrep $4 {} \;" >${5}$$-NODE'\
+                                            -- $log_path $within_days $log_file $key_words $output_file
 
     echo -e "line:${LINENO} ${FUNCNAME[0]} - END"
 }
 
-if [ ! -s $MACHINES ]
-then
-    echo "error $MACHINES"
-    exit
-fi
+[ ! -s $MACHINES ] && echo "error $MACHINES" && exit
 
 repo_history=${WORK_DIR}/repo_gc.reclaimed_history
 
 search_logs cm-chunk-reclaim.log 1 "RepoReclaimer.*successfully.recycled.repo" ${repo_history}-
-#awk -F':' '{ print $2 }' ${repo_history}-* | sed -e 's/ /T/g' | sort | uniq -c > ${repo_history}
-awk -F: '{count[$1]++} END{for(i in count) printf "%d\t%s\n",count[i],i}' > ${repo_history}
 
-if [[ ! -s ${repo_history} ]] ; then
-    echo
-    echo -e "line:${LINENO} ${FUNCNAME[0]} - ====> Repo GC Reclaim History"
-    echo -e "line:${LINENO} ${FUNCNAME[0]} -   There's no garbage reclaimed in past 24 hrs"
-fi
-
-awk 'BEGIN{
-        current_timestamp = mktime(strftime("%Y %m %d %H 00 00", systime()))
-    }{
-        chunk_num = $1
-        time_readable = $2
-        gsub(/-/," ",time_readable)
-        gsub(/T/," ",time_readable)
-        hr_delta = int((current_timestamp - mktime(time_readable" 00 00"))/(60*60))
-        hr_chunk_map[hr_delta] = chunk_num
-        hr_ts_map[hr_delta] = $2
-    } END{
-        print ""
-        printf("\033[1;34m%s\033[0m\n", "====> Repo GC Reclaim History")
-        chunk_cnt=0
-        for (hr_delta in hr_chunk_map) {
-            chunk_cnt += hr_chunk_map[hr_delta]
-            printf("  - In past %2s hrs reclaimed %6d chunks(%8.2f GB); In %s reclaimed %5d chunks(%7.2f GB)\n", hr_delta, chunk_cnt, 134217600*chunk_cnt/(1024*1024*1024), hr_ts_map[hr_delta], hr_chunk_map[hr_delta], 134217600*hr_chunk_map[hr_delta]/(1024*1024*1024))
-        }
-        print "  * Size is estimated with maximum chunk size, actual reclaimed size could be smaller"
-    }' ${repo_history}
+awk -F: '{
+            count[$1]++
+         } END{
+            printf("\033[1;34m%s\033[0m\n", "====> Repo GC Reclaim History")
+            for(i in count) {
+                chunk_sum += count[i]
+                printf("%s reclaimed %6d chunks(%8.2f GB), total reclaimed %5d chunks(%7.2f GB)\n",\
+                       i,count[i],count[i]*134217600/(1024*1024*1024),chunk_sum,chunk_sum*134217600/(1024*1024*1024));
+                print "  * Size is estimated with maximum chunk size, actual reclaimed size could be smaller"
+            }
+         }' ${repo_history}-*
 
