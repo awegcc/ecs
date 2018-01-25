e30cb61d5ed6a8477893b88a8e09701b8f39d265
dawtercc <dawtercc@gmail.com>
2018-01-25 03:09:38 +0000

    add.......

--- gc_reclaim_history.sh
+++ gc_reclaim_history.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 WORK_DIR=`pwd`
-MACHINES=MACHINE
+MACHINES=MACHINES
 
 repo_keywords='RepoReclaimer.*successfully.recycled.repo'
 btree_keywords='ReclaimState.*Chunk.*reclaimed:true'
@@ -18,27 +18,22 @@ xargs -a ${MACHINES} -I NODE -P0 sh -c \
            -- $log_path $within_days $log_file $repo_keywords $btree_keywords $output_file
 
 
-
-# 2018-01-23T22:05:01,239 [TaskScheduler-ChunkManager-DEFAULT_BACKGROUND_OPERATION-ScheduledExecutor-173]  INFO  ReclaimState.java (line 45) Chunk f181057c-6fa3-4f4c-9612-3d7b438cd459 reclaimed:true
-# 2017-06-30T20:06:23,163 [TaskScheduler-ChunkManager-DEFAULT_BACKGROUND_OPERATION-ScheduledExecutor-234]  INFO  RepoReclaimer.java (line 649) successfully recycled repo 3a0a0535-5d00-47d5-a293-96cfb93a0c59
 awk -F: '/ReclaimState.java/{
             count[$1]["btree"]++
          }
          /RepoReclaimer.java/{
             count[$1]["repo"]++
          } END{
-            printf("%-14s  %21s  %21s  %21s  %21s\n","time",
-                   "btreeCount(size GB)","repoCount(size GB)","btreeTotal(size GB)","repoTotal(size GB)")
+            printf("%-14s %28s %28s\n","time","btreeCount( total size GB )","repoCount( total size GB )")
             n=asorti(count,sorted)
             chunk_size=134217600/(1024*1024*1024)
             for(i=1;i<=n;i++){
-                btree_chunk_sum += count[sorted[i]]["btree"]
-                repo_chunk_sum += count[sorted[i]]["repo"]
-                printf("%-14s  %8d(%7.2f)  %8d(%7.2f) %12d(%9.2f) %12d(%9.2f)\n",\
+                btree_sum += count[sorted[i]]["btree"]
+                repo_sum += count[sorted[i]]["repo"]
+                printf("%-14s %12d(%12.2f ) %12d(%12.2f )\n",\
                        sorted[i],count[sorted[i]]["btree"],count[sorted[i]]["btree"]*chunk_size,\
-                       count[sorted[i]]["repo"],count[sorted[i]]["repo"]*chunk_size,\
-                       btree_chunk_sum,btree_chunk_sum*chunk_size,\
-                       repo_chunk_sum,repo_chunk_sum*chunk_size);
+                       count[sorted[i]]["repo"],count[sorted[i]]["repo"]*chunk_size)
             }
+            printf("%-14s %12d(%12.2f ) %12d(%12.2f )\n","summary",btree_sum,btree_sum*chunk_size,repo_sum,repo_sum*chunk_size)
          }' ${output_file}-*
 
