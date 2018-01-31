018080de678093cb2ba1da1c2564b43e0816b1fe
dawtercc <dawtercc@gmail.com>
2018-01-31 03:28:20 +0000

    replace dos2unix with sed

--- check_busyblock.sh
+++ check_busyblock.sh
@@ -10,7 +10,7 @@ fi
 curl -s "http://${ip_port}/diagnostic/SS/1/DumpAllKeys/SSTABLE_KEY?type=device&useStyle=raw" | grep -B1 schemaType > SS_device
 # http://10.240.29.31:9101/urn:storageos:OwnershipInfo:79634a09-0dad-44be-bee1-7a5766947a43__SS_11_128_1:/SSTABLE_KEY/?type=device
 # schemaType SSTABLE_KEY type DEVICE device 10.240.29.36
-dos2unix -q SS_device
+sed -i 's/\r$//' SS_device
 awk -F'[ ?]' '/http/{url=$1}/schemaType/{printf("%s?device=%s&showvalue=gpb&type=PARTITION\n",url,$6)}' SS_device > SS_device_partition
 # http://10.240.29.33:9101/urn:storageos:OwnershipInfo:79634a09-0dad-44be-bee1-7a5766947a43__SS_19_128_1:/SSTABLE_KEY/?type=PARTITION&device=10.240.29.31&showvalue=gpb
 # http://10.240.29.33:9101/urn:storageos:OwnershipInfo:79634a09-0dad-44be-bee1-7a5766947a43__SS_19_128_1:/SSTABLE_KEY/?device=10.240.29.31&showvalue=gpb&type=PARTITION
