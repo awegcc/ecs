47a81711702a44ef71173f0a5229ade5b5a16486
dawtercc <dawtercc@gmail.com>
2018-02-07 03:09:25 +0000

    refine check cleanup job

--- check_cleanupjob.sh
+++ check_cleanupjob.sh
@@ -1,21 +1,21 @@
-#/bin/sh
-#
+#!/bin/sh
 if [[ $# < 1 ]]
 then
     ip_port=$(netstat -ntl | awk '/:9101/{print $4;exit}')
 else
     ip_port="$1:9101"
 fi
-
-postfix='DELETE_JOB_TABLE_KEY?type=CLEANUP_JOB&objectId=aa&maxkeys=1&useStyle=raw'
-url_addr="http://${ip_port}/diagnostic/OB/0/"
-
-curl -s "$url_addr" | xmllint --format - | awk -F'[<>?]' '/table_detail_link/{print $3}' | while read url
-do
-    curl -s -L "${url}${postfix}" | grep schemaType
-done > cleanupjobs.lst
-
-#sh /tmp/cleanup_job_command.sh.2 > /tmp/cleanupjob.tmp
-
-#grep schemaType /tmp/cleanupjob.tmp | awk '{print $4}' | sort -n | head -3
+cleanupjobs=cleanupjob.$(date +'%Y%m%d-%H%M')
+suffix='DELETE_JOB_TABLE_KEY?type=CLEANUP_JOB&objectId=aa&maxkeys=1&useStyle=raw'
+if [ ! -s cleanupjob_command.sh ]
+then
+  curl "http://${ip_port}/diagnostic/OB/0" | xmllint --format - | awk -F'[<>?]' '/table_detail_link/{print $3}' > obdt_url.list
+  while read url
+  do
+    echo echo $url
+    echo curl -s \"${url}${suffix}\"
+  done < obdt_url.list > cleanupjob_command.sh
+fi
+sh cleanupjob_command.sh > $cleanupjobs
+grep 'schemaType' $cleanupjobs | sort -k3 -n | head -4
 
