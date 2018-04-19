1c36da27c9a5ab6ff673e936db17824058854cd8
dawtercc <dawtercc@gmail.com>
2018-04-19 06:16:34 +0000

    tweak

--- gc_reclaim_stat.sh
+++ gc_reclaim_stat.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+# ver: 2
 days=1
 WORK_DIR="`pwd`/reclaim"
 log_file='cm-chunk-reclaim.log*'
 output_file=${WORK_DIR}/gc.reclaimed_history
-#btree_keywords='ReclaimState.*Chunk.*reclaimed:true'
 btree_keywords='Running.state.RECLAIM'
 repo_keywords='RepoReclaimer.*successfully.recycled.repo'
 log_path='/opt/emc/caspian/fabric/agent/services/object/main/log/'
@@ -20,27 +20,25 @@ do
       ;;
     esac
 done
-((${#ip_port}<12)) && ip_port=$(netstat -ntl | awk '/:9101/{print $4;exit}')
 [ -d $WORK_DIR ] && rm -f ${output_file}* || mkdir $WORK_DIR
-
+((${#ip_port}<12)) && ip_port=$(netstat -ntl | awk '/:9101/{print $4;exit}')
 MACHINES=".${ip_port%:*}.ip"
 if [ ! -s $MACHINES ]
 then
     curl -s "http://${ip_port}/diagnostic/RT/0/" | xmllint --format - | awk -F'[<>]' '/owner_ipaddress/{ip[$3]++}END{for(k in ip)print k}' > $MACHINES
 fi
 
-echo "Counting past $days day(s) reclaimed chunks and size( chunks #*128M )"
-
+echo "Counting past $days day(s) reclaimed chunks and total size( chunks #*128M )"
 xargs -a ${MACHINES} -I NODE -P0 sh -c \
            'ssh NODE "find $1 -maxdepth 1 -mtime -$2 -name \"$3\" -exec zgrep \"$4\\|$5\" {} \;" >${6}-NODE 2>/dev/null'\
            -- $log_path $days "$log_file" "$repo_keywords" "$btree_keywords" "$output_file"
 
-awk -F: '/ReclaimState.java/{
+awk -F: '/BPlusTreeChunkReclaimStateMachine.java/ {
             count[$1]["btree"]++
          }
-         /RepoReclaimer.java/{
+         /RepoReclaimer.java/ {
             count[$1]["repo"]++
-         } END{
+         } END {
             printf("%-15s %26s %27s\n","date-time","btreeChunks(total size GB)","repoChunks(total size GB)")
             n=asorti(count,sorted)
             chunk_size=134217600/(1024*1024*1024)
